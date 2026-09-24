406cbda84d230167692882a460e17eef93e74bb5
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-24 23:03:48 +0200

    Created PersonModel and Extension class for person model

---
+++ ExtensionMethodsApp/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethodsApp
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ ExtensionMethodsApp/PersonModelExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtensionMethodsApp
+{
+    public static class PersonModelExtension
+    {
+        public static PersonModel SetDefaultAge(this PersonModel person)
+        {
+            person.Age = 18;
+            return person;
+        }
+
+        public static string PrintInfo(this PersonModel person)
+        {
+            return $"{person.FirstName} {person.LastName} is {person.Age}";
+        }
+    }
+}
